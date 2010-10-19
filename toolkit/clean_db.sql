23533fba18c4cf1b01914a94f897fe13843d478c
Chris Keller <xylo04@gmail.com>
2010-10-19 16:24:28 -0600

    added toolkit directory, and htaccess file to prevent visibility on web server

---
+++ toolkit/clean_db.sql
@@ -0,0 +1,16 @@
+/* First, create our posts table: */
+CREATE TABLE posts (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(50),
+    body TEXT,
+    created DATETIME DEFAULT NULL,
+    modified DATETIME DEFAULT NULL
+);
+
+/* Then insert some posts for testing: */
+INSERT INTO posts (title,body,created)
+    VALUES ('The title', 'This is the post body.', NOW());
+INSERT INTO posts (title,body,created)
+    VALUES ('A title once again', 'And the post body follows.', NOW());
+INSERT INTO posts (title,body,created)
+    VALUES ('Title strikes back', 'This is really exciting! Not.', NOW());
